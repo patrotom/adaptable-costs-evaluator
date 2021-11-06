6f714c3c4fa08d4454ebd7b79799ab01d2d3d3c5
Tomas Patro <tomas.patro@gmail.com>
2021-11-06 11:44:30 +0100

    Update field schemas test

--- test/adaptable_costs_evaluator/field_schemas_test.exs
+++ test/adaptable_costs_evaluator/field_schemas_test.exs
@@ -1,65 +1,65 @@
 defmodule AdaptableCostsEvaluator.FieldSchemasTest do
   use AdaptableCostsEvaluator.DataCase
+  use AdaptableCostsEvaluator.Fixtures.{FieldSchemaFixture}
 
   alias AdaptableCostsEvaluator.FieldSchemas
 
   describe "field_schemas" do
     alias AdaptableCostsEvaluator.FieldSchemas.FieldSchema
 
-    @valid_attrs %{definition: %{}, name: "some name"}
-    @update_attrs %{definition: %{}, name: "some updated name"}
-    @invalid_attrs %{definition: nil, name: nil}
-
-    def field_schema_fixture(attrs \\ %{}) do
-      {:ok, field_schema} =
-        attrs
-        |> Enum.into(@valid_attrs)
-        |> FieldSchemas.create_field_schema()
-
-      field_schema
+    setup do
+      %{field_schema: field_schema_fixture()}
     end
 
-    test "list_field_schemas/0 returns all field_schemas" do
-      field_schema = field_schema_fixture()
+    test "list_field_schemas/0 returns all field_schemas", %{field_schema: field_schema} do
       assert FieldSchemas.list_field_schemas() == [field_schema]
     end
 
-    test "get_field_schema!/1 returns the field_schema with given id" do
-      field_schema = field_schema_fixture()
+    test "get_field_schema!/1 returns the field_schema with given id", %{
+      field_schema: field_schema
+    } do
       assert FieldSchemas.get_field_schema!(field_schema.id) == field_schema
     end
 
     test "create_field_schema/1 with valid data creates a field_schema" do
-      assert {:ok, %FieldSchema{} = field_schema} = FieldSchemas.create_field_schema(@valid_attrs)
+      attrs = %{@valid_attrs | name: "custom name"}
+
+      assert {:ok, %FieldSchema{} = field_schema} = FieldSchemas.create_field_schema(attrs)
       assert field_schema.definition == %{}
-      assert field_schema.name == "some name"
+      assert field_schema.name == "custom name"
     end
 
     test "create_field_schema/1 with invalid data returns error changeset" do
       assert {:error, %Ecto.Changeset{}} = FieldSchemas.create_field_schema(@invalid_attrs)
     end
 
-    test "update_field_schema/2 with valid data updates the field_schema" do
-      field_schema = field_schema_fixture()
-      assert {:ok, %FieldSchema{} = field_schema} = FieldSchemas.update_field_schema(field_schema, @update_attrs)
+    test "update_field_schema/2 with valid data updates the field_schema", %{
+      field_schema: field_schema
+    } do
+
+      assert {:ok, %FieldSchema{} = field_schema} =
+               FieldSchemas.update_field_schema(field_schema, @update_attrs)
+
       assert field_schema.definition == %{}
       assert field_schema.name == "some updated name"
     end
 
-    test "update_field_schema/2 with invalid data returns error changeset" do
-      field_schema = field_schema_fixture()
-      assert {:error, %Ecto.Changeset{}} = FieldSchemas.update_field_schema(field_schema, @invalid_attrs)
+    test "update_field_schema/2 with invalid data returns error changeset", %{
+      field_schema: field_schema
+    } do
+
+      assert {:error, %Ecto.Changeset{}} =
+               FieldSchemas.update_field_schema(field_schema, @invalid_attrs)
+
       assert field_schema == FieldSchemas.get_field_schema!(field_schema.id)
     end
 
-    test "delete_field_schema/1 deletes the field_schema" do
-      field_schema = field_schema_fixture()
+    test "delete_field_schema/1 deletes the field_schema", %{field_schema: field_schema} do
       assert {:ok, %FieldSchema{}} = FieldSchemas.delete_field_schema(field_schema)
       assert_raise Ecto.NoResultsError, fn -> FieldSchemas.get_field_schema!(field_schema.id) end
     end
 
-    test "change_field_schema/1 returns a field_schema changeset" do
-      field_schema = field_schema_fixture()
+    test "change_field_schema/1 returns a field_schema changeset", %{field_schema: field_schema} do
       assert %Ecto.Changeset{} = FieldSchemas.change_field_schema(field_schema)
     end
   end
