eb52145785c137e35665280e2a01e23c710c3b3c
Tomáš Patro <tomas.patro@gmail.com>
2021-11-14 13:19:59 +0100

    Dockerize Application for Development and Test (#23)
    * Add Dockerfile, docker-compose, and set necessary configs
    * Enhance devops stuff
    * Edit PGPORT env

---
+++ Dockerfile
@@ -0,0 +1,22 @@
+# Extend from the official Elixir image
+FROM elixir:1.12.3
+
+# Create app directory and copy the Elixir projects into it
+RUN mkdir /app
+COPY . /app
+WORKDIR /app
+
+# Install OS packages
+RUN apt update
+RUN apt install --yes build-essential inotify-tools postgresql-client
+
+# Install Elixir packages
+RUN mix local.hex --force
+RUN mix local.rebar --force
+
+# Get the dependencies and compile the project
+RUN mix deps.get
+RUN mix do compile
+
+# Run the app
+CMD ["/app/scripts/entrypoint.sh"]
--- config/test.exs
+++ config/test.exs
@@ -1,3 +1,4 @@
+# Test environment configuration
 use Mix.Config
 
 # Configure your database
@@ -6,16 +7,18 @@ use Mix.Config
 # to provide built-in test partitioning in CI environment.
 # Run `mix help test` for more information.
 config :adaptable_costs_evaluator, AdaptableCostsEvaluator.Repo,
-  username: "postgres",
-  password: "postgres",
-  database: "adaptable_costs_evaluator_test#{System.get_env("MIX_TEST_PARTITION")}",
-  hostname: "localhost",
+  username: System.get_env("PGUSER", "postgres"),
+  password: System.get_env("PGPASSWORD", "postgres"),
+  database:
+    "#{System.get_env("PGDATABASE_TEST", "adaptable_costs_evaluator_test")}#{System.get_env("MIX_TEST_PARTITION")}",
+  hostname: System.get_env("PGHOST", "localhost"),
+  port: System.get_env("PGPORT", "5432"),
   pool: Ecto.Adapters.SQL.Sandbox
 
 # We don't run a server during test. If one is required,
 # you can enable the server option below.
 config :adaptable_costs_evaluator, AdaptableCostsEvaluatorWeb.Endpoint,
-  http: [port: 4002],
+  http: [port: System.get_env("TEST_APP_PORT", "4002")],
   server: false
 
 # Print only warnings and errors during test
